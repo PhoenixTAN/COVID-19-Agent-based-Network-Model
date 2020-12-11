31dd0fb436a469dd0372244df15d7e6e623d14ed
Tian <dingtian2019@163.com>
2020-12-11 12:53:13 -0500

    Event: change parameters name

--- Event/header/Event.h
+++ Event/header/Event.h
@@ -23,23 +23,23 @@ public:
   uint32_t getDead();
 
   //setters
-  void setSusceptible(uint32_t Susceptible);
-  void setPresymptomatic(uint32_t Presymptomatic);
-  void setAsymptomatic(uint32_t Asymptomatic);
-  void setMild(uint32_t Mild);
-  void setRecovered(uint32_t Recovered);
-  void setSevere(uint32_t Severe);
-  void setDead(uint32_t Dead);
+  void setSusceptible(uint32_t num_of_susceptible);
+  void setPresymptomatic(uint32_t num_of_presymptomatic);
+  void setAsymptomatic(uint32_t num_of_asymptomatic);
+  void setMild(uint32_t num_of_mild);
+  void setRecovered(uint32_t num_of_recovered);
+  void setSevere(uint32_t num_of_severe);
+  void setDead(uint32_t num_of_dead);
 
 
 private:
   //parameters
-  uint32_t Susceptible;
-  uint32_t Presymptomatic;
-  uint32_t Asymptomatic;
-  uint32_t Mild;
-  uint32_t Recovered;
-  uint32_t Severe;
-  uint32_t Dead;
+  uint32_t num_of_susceptible;
+  uint32_t num_of_presymptomatic;
+  uint32_t num_of_asymptomatic;
+  uint32_t num_of_mild;
+  uint32_t num_of_recovered;
+  uint32_t num_of_severe;
+  uint32_t num_of_dead;
 };
 #endif
